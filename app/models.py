9fabc4678b9d53d19c2ad437407b8ff6ed45fdd2
Daniel Kukulka <dkukulka72@gmail.com>
2026-05-28 13:41:27 -0500

    Add SQLAlchemy and Flask-Migrate setup

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from app.extensions import db
+
+
+class FoodEntry(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    log_date = db.Column(db.String(50), nullable=False)
+
+    food = db.Column(db.String(100), nullable=False)
+
+    calories = db.Column(db.Float, nullable=False)
+
+    protein = db.Column(db.Float, nullable=False)
+
+    carbs = db.Column(db.Float, nullable=False)
+
+    fats = db.Column(db.Float, nullable=False)
+
+
+class Goal(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    calorie_goal = db.Column(db.Float, nullable=False)
+
+    protein_goal = db.Column(db.Float, nullable=False)
---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = "dev-secret-key"
+
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + os.path.join(basedir, "nutrition_log.db")
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
